64b83da81c561333b2760d06ecd56a448a83f85b
Yuya Hirano <v322sk@gmail.com>
2023-01-07 13:22:18 -0800

    refactoring

--- src/components/Works/WorksMain.tsx
+++ src/components/Works/WorksMain.tsx
@@ -4,39 +4,96 @@ import work1img from "../../assets/rob-mulally-oacHEtIlXsA-unsplash.jpg";
 const WorksMain: React.FC = () => {
   return (
     <>
-      <div className="content">
-        <div className="articles">
-          {/* <div className="title">Works</div> */}
-          <hr></hr>
-          <div className="works">
-            <div className="work">
-              <img src={work1img} className="workimg" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Numquam, aliquam dolorum commodi adipisci ipsam officiis,
-                perspiciatis voluptas fugit ab sunt modi magni? Commodi
-                laudantium placeat illo nam magni soluta repudiandae!
-              </p>
-            </div>
-            <div className="work">
-              <img src={work1img} className="workimg" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Numquam, aliquam dolorum commodi adipisci ipsam officiis,
-                perspiciatis voluptas fugit ab sunt modi magni? Commodi
-                laudantium placeat illo nam magni soluta repudiandae!
-              </p>
-            </div>
-            <div className="work">
-              <img src={work1img} className="workimg" />
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Numquam, aliquam dolorum commodi adipisci ipsam officiis,
-                perspiciatis voluptas fugit ab sunt modi magni? Commodi
-                laudantium placeat illo nam magni soluta repudiandae!
-              </p>
+      <div className="work-content">
+        <div className="work-title">
+          <h1>Things I have been working on</h1>
+        </div>
+        <div className="works">
+          <div className="work">
+            <img src={work1img} className="work-image" />
+            <div className="detail">
+              <div className="work-text">
+                <h3>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquam dolorum commodi adipisci
+                  ipsam officiis, perspiciatis voluptas fugit ab sunt modi magni? Commodi laudantium placeat illo nam
+                  magni soluta repudiandae!
+                </h3>
+              </div>
+              <div className="stacks">
+                <h3>react typescript nextjs tailwindcss</h3>
+              </div>
+              <div className="work-links">
+                <a href="#" target="_blank" rel="">
+                  VIEW CODE
+                </a>
+                <a href="#" target="_blank" rel="">
+                  VISIT APP
+                </a>
+              </div>
             </div>
           </div>
+          <div className="work">
+            <img src={work1img} className="work-image" />
+            <div className="detail">
+              <div className="work-text">
+                <h3>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquam dolorum commodi adipisci
+                  ipsam officiis, perspiciatis voluptas fugit ab sunt modi magni? Commodi laudantium placeat illo nam
+                  magni soluta repudiandae!
+                </h3>
+              </div>
+              <div className="stacks">
+                <h3>react typescript nextjs tailwindcss</h3>
+              </div>
+              <div className="work-links">
+                <a href="#" target="_blank" rel="">
+                  VIEW CODE
+                </a>
+                <a href="#" target="_blank" rel="">
+                  VISIT APP
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="work">
+            <img src={work1img} className="work-image" />
+            <div className="detail">
+              <div className="work-text">
+                <h3>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquam dolorum commodi adipisci
+                  ipsam officiis, perspiciatis voluptas fugit ab sunt modi magni? Commodi laudantium placeat illo nam
+                  magni soluta repudiandae!
+                </h3>
+              </div>
+              <div className="stacks">
+                <h3>react typescript nextjs tailwindcss</h3>
+              </div>
+              <div className="work-links">
+                <a href="#" target="_blank" rel="">
+                  VIEW CODE
+                </a>
+                <a href="#" target="_blank" rel="">
+                  VISIT APP
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* <div className="work">
+            <img src={work1img} className="workimg" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquam dolorum commodi adipisci ipsam
+              officiis, perspiciatis voluptas fugit ab sunt modi magni? Commodi laudantium placeat illo nam magni soluta
+              repudiandae!
+            </p>
+          </div>
+          <div className="work">
+            <img src={work1img} className="workimg" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, aliquam dolorum commodi adipisci ipsam
+              officiis, perspiciatis voluptas fugit ab sunt modi magni? Commodi laudantium placeat illo nam magni soluta
+              repudiandae!
+            </p>
+          </div> */}
         </div>
       </div>
     </>
