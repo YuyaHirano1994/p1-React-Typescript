c42d298c158ee020c3b4c3986ad989967ed8f658
Yuya Hirano <v322sk@gmail.com>
2023-01-07 18:20:42 -0800

    refactoring

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -11,16 +11,6 @@ const Footer = () => {
             <Icon icon="akar-icons:instagram-fill" />
           </a>
         </li>
-        <li id="github">
-          <a href="https://github.com/YuyaHirano1994" target="_blank" rel="noreferrer">
-            <Icon icon="akar-icons:github-fill" />
-          </a>
-        </li>
-        <li id="linkedin">
-          <a href="https://www.linkedin.com/in/yuya-hirano-9605a5247/" target="_blank" rel="noreferrer">
-            <Icon icon="akar-icons:linkedin-box-fill" />
-          </a>
-        </li>
         <li id="resume">
           <a
             href="https://docs.google.com/document/d/1VAN6QC48I2LpRTR3v4zbU8_aWCt4TmvCyP41JdAs73U/edit?usp=share_link"
